129eaf7e391cf561587d8eebe90a0e231be625f3
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2022-08-30 14:06:04 -0300

    Update ex1.s

--- Exercicios/ex1.s
+++ Exercicios/ex1.s
@@ -3,21 +3,20 @@
 .text
 
 main:
-
-	li $v0, #pegar um inteiro
-	syscall
-        move $t0,$v0
-
-	li $v0, #pegar um inteiro
-	syscall
-        move $t1,$v0
+	li $t0 , 50
+	sw $t0,40($sp)#regiao da pilha pra colocar dados posicao 10
 	
-	sub $t2,$t0,$t1
+	li $t1,100
+	sw $t1,56($sp)#regiao da pilha pra colocar dados posicao 14
 	
-
-	#mostrar resultado
+	#pegar os valores da memoria e trazer pro registrador
+	lw $t2,40($sp) 
+	lw $t3,160($sp)
+	
+	add $t4,$t2,$t3
+	
 	li $v0,1
-	move $a0,$t2
+	move $a0,$t4
 	syscall
 	
 	li $v0,10
